fcb0aec9798e7835478de3fe2c49d6f0e82daf28
fanyrodrigues <fantinny.santos@etec.sp.gov.br>
2025-02-14 11:58:16 -0300

    Fany

---
+++ Projeto-the-games/app.py
@@ -0,0 +1,24 @@
+#Importando o pacote do frask
+from flask import Flask
+#carregando o frask na  variável app, não precisa informar o tipo de variável
+#dois underline
+app = Flask(__name__)
+@app.route ('/')
+# criando função
+def home():
+    return '<h1> O meu primeiro site em Flask. Bem vindos!!!! <br>  </h1>'
+
+@app.route ('/games')
+
+def games():
+    return '<h1> Seja bem vindo a página de games !! <3 <br>  </h1>'
+
+
+if __name__ ==  '__main__':
+#nome = "Fany"
+#numero = 12 debug => decoração, true é ativado. Ele avisa os erro. Rodando o servidor no localhost, porta 5000. POde ser executado no terminal : python app.py
+
+ app.run(host='localhost', port=5000, debug=True ) 
+ 
+ #Rota 5 www.site, com ('/') <= principal
+# www.site.com/produtos (/produtos)
